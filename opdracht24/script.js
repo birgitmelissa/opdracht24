625d512ab23b06b039dd787f21525e78a6bc74ad
birgitmelissa <94128573+birgitmelissa@users.noreply.github.com>
2021-12-24 14:30:05 +0100

    Add files via upload

---
+++ opdracht24/script.js
@@ -0,0 +1,20 @@
+function add(nr1, nr2) {
+    return nr1 + nr2;
+  }
+  
+const result = add(4, 8);
+console.log(result)
+
+const optellen = function (number1, number2) {
+    return number1 + number2;
+  };
+  
+const uitkomst = optellen(4, 8);
+console.log(uitkomst)
+
+const agregar = (n1, n2) => {
+    return n1 + n2;
+  };
+
+const salir = agregar(4, 8);
+console.log(salir)
